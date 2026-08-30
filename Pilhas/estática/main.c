e4903d24eaad0acfc6d19b9df2485969af4c277e
GabrielVPalma <gabriel2017palma@gmail.com>
2026-08-30 01:27:24 -0300

    correção da leitura indevida de strings pelo getchar

--- Pilhas/estática/main.c
+++ Pilhas/estática/main.c
@@ -6,7 +6,7 @@ int main(){
 
     Pilha pilha;
     Acao acao;
-    int opcao;
+    int opcao = -1;
 
     inicializarPilha(&pilha);
 
@@ -19,59 +19,71 @@ int main(){
         printf("5 - Limpar historico\n");
         printf("0 - Sair\n");
         printf("Escolha uma opcao: ");
-        scanf("%d", &opcao);
-        getchar();
 
-        switch(opcao){
-            case 1:
-            printf("\nDigite a descricao da acao: ");
-            fgets(acao.descricao, sizeof(acao.descricao), stdin);
-            
-            acao.descricao[strcspn(acao.descricao, "\n")] = '\0';
-
-            if(push(&pilha, acao)){
-                printf("Acao adicionada com sucesso!\n");
-            }else{
-                printf("Nao foi possivel adicionar a acao. A pilha esta cheia!\n");
-            }
-
-            break;
-
-            case 2:
-            if(pop(&pilha, &acao)){
-                printf("\nAcao desfeita: %s\n", acao.descricao);
-            }else{
-                printf("\nNao foi possivel desfazer. A pilha esta vazia!\n");
-            }
-
-            break;
-
-            case 3:
-            if(top(&pilha, &acao)){
-                printf("\nUltima acao: %s\n", acao.descricao);
-            }else{
-                printf("\nA pilha esta vazia!\n");
-            }
-
-            break;
-
-            case 4:
-            listarPilha(&pilha);
-            break;
-
-            case 5:
-            limparPilha(&pilha);
-            printf("\nHistorico limpo com sucesso!\n");
-            break;
-            
-            case 0:
-            printf("\nPrograma encerrado...\n");
-            break;
-
-            default:
+        if(scanf("%d", &opcao) != 1){
             printf("\nOpcao invalida!\n");
+            printf("Digite apenas numeros...\n");
+
+            while(getchar() != '\n');
+
+            continue;
         }
 
+        getchar();
+
+        if(opcao >= 0 && opcao <= 5){
+            switch(opcao){
+                case 1:
+                printf("\nDigite a descricao da acao: ");
+                fgets(acao.descricao, sizeof(acao.descricao), stdin);
+            
+                acao.descricao[strcspn(acao.descricao, "\n")] = '\0';
+
+                if(push(&pilha, acao)){
+                    printf("Acao adicionada com sucesso!\n");
+                }else{
+                    printf("Nao foi possivel adicionar a acao. A pilha esta cheia!\n");
+                }
+
+                break;
+
+                case 2:
+                if(pop(&pilha, &acao)){
+                    printf("\nAcao desfeita: %s\n", acao.descricao);
+                }else{
+                    printf("\nNao foi possivel desfazer. A pilha esta vazia!\n");
+                }
+
+                break;
+
+                case 3:
+                if(top(&pilha, &acao)){
+                    printf("\nUltima acao: %s\n", acao.descricao);
+                }else{
+                    printf("\nA pilha esta vazia!\n");
+                }
+
+                break;
+
+                case 4:
+                listarPilha(&pilha);
+                break;
+
+                case 5:
+                limparPilha(&pilha);
+                printf("\nHistorico limpo com sucesso!\n");
+                break;
+            
+                case 0:
+                printf("\nPrograma encerrado...\n");
+                break;
+            }
+
+        }else{
+            printf("\n\nOpcao invalida!\n");
+            printf("Tente novamente...\n");
+        }
+        
     } while(opcao != 0);
 
     return 0;
