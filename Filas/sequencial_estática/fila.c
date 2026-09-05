e8d561c2960ebfbafb7b3a54034921aa9790adb0
GabrielVPalma <gabriel2017palma@gmail.com>
2026-09-05 15:46:26 -0300

    Implementação fila.c

---
+++ Filas/sequencial_estática/fila.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "fila.h"
+
+void inicializarFila(Fila *fila){
+    fila->inicio = 0;
+    fila->fim = 0;
+}
+
+int enfileirar(Fila *fila, Cliente cliente){
+    if(filaCheia(fila)){
+        return 0;
+    }
+
+    fila->elementos[fila->fim] = cliente;
+    fila->fim++;
+
+    return 1;
+}
+
+int desenfileirar(Fila *fila, Cliente *cliente){
+    int (filaVazia(fila)){
+        return 0;
+    }
+
+    *cliente = fila->elementos[fila->inicio];
+    fila->inicio++;
+
+    return 1;
+}
+
+int frente(Fila *fila, Cliente *cliente){
+    if(filaVazia(fila)){
+        return 0;
+    }
+
+    *cliente = fila->elementos[fila->inicio];
+
+    return 1;
+}
+
+int filaVazia(Fila *fila){
+    return fila->inicio == fila->fim;
+}
+
+int filaCheia(Fila *fila){
+    return fila->fim == MAX;
+}
+
+int listarFila(Fila *fila){
+    if(filaVazia(fila)){
+        printf("\nA fila esta vazia!\n");
+        return;
+    }
+
+    printf("\n===== FILA DE ATENDIMENTO =====\n");
+
+    for(int i = fila->inicio; i < fila->fim; i++){
+        printf("Senha: %d | Nome: %s\n", fila->elemento[i].senha, fila->elemento[i].nome);
+    }
+}
+
+void limparFila(Fila *fila){
+    fila->inicio = 0;
+    fila->fim = 0;
+}
