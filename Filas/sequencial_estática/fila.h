112b3d2ba668810b0c4d7802adfe9b761f72e392
GabrielVPalma <gabriel2017palma@gmail.com>
2026-09-05 15:28:30 -0300

    Implementação fila.h

---
+++ Filas/sequencial_estática/fila.h
@@ -0,0 +1,36 @@
+#ifndef FILA_H
+#define FILA_H
+
+#define MAX 5
+
+typedef struct{
+    int senha;
+    char nome[50];
+} Cliente;
+
+typedef struct{
+    Cliente elementos[MAX];
+    int inicio;
+    int fim;
+} Fila;
+
+void inicializarFila(Fila *fila);
+
+//Adiciona um cliente ao final da fila
+int enfileirar(Fila *fila, Cliente cliente);
+
+//Remove o primeiro cliente da fila
+int desenfileirar(Fila *fila, Cliente *cliente);
+
+//Consulta o primeiro cliente sem removê-lo
+int frente(Fila *fila, Cliente *cliente);
+
+int filaVazia(Fila *fila);
+
+int filaCheia(Fila *fila);
+
+void listarFila(Fila *fila);
+
+void limparFila(Fila *fila);
+
+#endif
